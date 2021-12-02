8c3266c1df26597a81a82a8551f1378628823938
Aslan <thebestdevelopering@gmail.com>
2021-12-02 17:32:16 +0300

    finalization of the slider

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const slides = document.querySelectorAll(".slide");
+function slidesPlugin(activeSlide) {
+  const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
-  function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove("active");
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
     });
+    function clearActiveClasses() {
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+    }
   }
 }
+
+slidesPlugin(4);
